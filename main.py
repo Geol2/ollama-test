93173750e0ccd1a20caee0aa75c2b462e192f3d2
Geol2 <big9401@gmail.com>
2026-05-19 18:00:20 +0900

    add : 의존성 추가

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import ollama
+
 def main():
     print("Hello from ollama!")
 
+    result = ollama.generate(model='qwen3.8b', prompt='왜 하늘은 파랗죠?')
+    print(result['response'])    
 
 if __name__ == "__main__":
     main()
